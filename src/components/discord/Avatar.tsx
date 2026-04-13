5b9767149da3f32a9a45f71c52e690eaae9d92f9
[dyad] <fafe.rug@gmail.com>
2026-04-13 02:27:32 +0200

    [dyad] Aggiunti i contorni Esplosivo e Hacker Matrix allo shop - wrote 3 file(s)

--- src/components/discord/Avatar.tsx
+++ src/components/discord/Avatar.tsx
@@ -15,11 +15,32 @@ export const Avatar = ({ src, alt, className = "", decoration }: AvatarProps) =>
   return (
     <div className={`relative rounded-full flex items-center justify-center dec-wrapper dec-${decoration} ${className}`}>
       <img src={src} alt={alt} className="w-full h-full rounded-full object-cover relative z-10" />
+      
       {decoration === 'dc-emit' && (
         <>
           <div className="dc-particle p1"></div>
           <div className="dc-particle p2"></div>
           <div className="dc-particle p3"></div>
+        </>
+      )}
+      
+      {decoration === 'matrix' && (
+        <>
+          <div className="matrix-char m1">1</div>
+          <div className="matrix-char m2">0</div>
+          <div className="matrix-char m3">1</div>
+          <div className="matrix-char m4">0</div>
+        </>
+      )}
+      
+      {decoration === 'explosive' && (
+        <>
+          <div className="explode-emoji e1">💥</div>
+          <div className="explode-emoji e2">🔥</div>
+          <div className="explode-emoji e3">💥</div>
+          <div className="explode-emoji e4">🧨</div>
+          <div className="explode-emoji e5">💥</div>
+          <div className="explode-emoji e6">🔥</div>
         </>
       )}
     </div>
